4876ebf0ccf84daec37c4162671ec93acbf2e394
GyaniHulk <adamya@appfoster.com>
2024-11-12 22:35:35 +0530

    fix:validation

--- src/schemas/product.ts
+++ src/schemas/product.ts
@@ -24,6 +24,7 @@ export default defineType({
       name: 'mainImage',
       title: 'Main image',
       type: 'image',
+      validation: (Rule) => Rule.required(),
       options: {
         hotspot: true,
       },
@@ -31,88 +32,105 @@ export default defineType({
     defineField({
       name: 'price',
       title: 'Price',
+      validation: (Rule) => Rule.required(),
       type: 'number',
     }),
     defineField({
       name: 'discountedPrice',
       title: 'Discounted Price',
+       validation: (Rule) => Rule.required(),
       type: 'number',
     }),
     defineField({
       name: 'photos',
       title: 'Photos',
       type: 'array',
+      validation: (Rule) => Rule.required(),
       of: [{ type: 'image' }],
     }),
     defineField({
       name: 'description',
       title: 'Description',
+      validation: (Rule) => Rule.required(),
       type: 'blockContent',
     }),
     defineField({
       name: 'features',
       title: 'Features',
+      validation: (Rule) => Rule.required(),
       type: 'blockContent',
     }),
     defineField({
       name: 'maxSpeed',
       title: 'Max Speed',
+      validation: (Rule) => Rule.required(),
       type: 'string',
     }),
     defineField({
       name: 'maxPower',
       title: 'Max Power',
+      validation: (Rule) => Rule.required(),
       type: 'string',
     }),
     defineField({
       name: 'cargoBoxDimension',
       title: 'Cargo Box Dimension',
+      validation: (Rule) => Rule.required(),
       type: 'string',
     }),
     defineField({
       name: 'rangeCharge',
       title: 'Range/Charge',
+      validation: (Rule) => Rule.required(),
       type: 'string',
     }),
     defineField({
       name: 'groundClearance',
       title: 'Ground Clearance',
+      validation: (Rule) => Rule.required(),
       type: 'string',
     }),
     defineField({
       name: 'gradeability',
       title: 'Gradeability',
+      validation: (Rule) => Rule.required(),
       type: 'string',
     }),
     defineField({
       name: 'gvw',
       title: 'GVW',
+      validation: (Rule) => Rule.required(),
       type: 'string',
     }),
     defineField({
       name: 'batteryCapacity',
       title: 'Battery Capacity',
+      validation: (Rule) => Rule.required(),
       type: 'string',
     }),
     defineField({
       name: 'batteryChargingTime',
       title: 'Battery Charging Time',
+      validation: (Rule) => Rule.required(),
       type: 'string',
     }),
     defineField({
       name: 'instrumentCluster',
       title: 'Instrument Cluster',
+      validation: (Rule) => Rule.required(),
       type: 'string',
     }),
     defineField({
       name: 'tyreSize',
       title: 'Tyre Size',
+      validation: (Rule) => Rule.required(),
       type: 'string',
     }),
     defineField({
       name: 'brake',
       title: 'Brake',
       type: 'string',
+      validation: (Rule) => Rule.required(),
     }),
   ],
   preview: {
@@ -120,6 +138,7 @@ export default defineType({
       title: 'title',
       author: 'author.name',
       media: 'mainImage',
+      
     },
     prepare(selection) {
       const { author } = selection
